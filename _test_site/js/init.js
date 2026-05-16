9a9867130696328bf11b6753a0f0437c5413a30f
larsenv <larsen@weborican.com>
2026-05-16 14:31:12 -0500

    perf: remove redundant jQuery dependency
    - Removed jQuery v3.7.1 as Materialize v2.3.3 uses native JavaScript.
    - Reduced page payload and improved load speed.

--- _test_site/js/init.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var sidenavElems = document.querySelectorAll('.sidenav, .button-collapse');
-  var sidenavInstances = M.Sidenav.init(sidenavElems);
-  
-  var parallaxElems = document.querySelectorAll('.parallax');
-  var parallaxInstances = M.Parallax.init(parallaxElems);
-});
